f0c35840345ee93670856b7e6d6a3284d7db6ecc
Kajal03 <connected.kajal@gmail.com>
2020-02-05 00:38:24 +0530

    add java and aos programs

---
+++ AOS/ch5-read.c
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	int fd;
+	char lilbuf[20], bigbuf[1024];
+
+	fd = open("in.txt", kajal) ;
+	read(fd, lilbuf, 20);
+	read(fd, bigbuf, 1024);
+	read(fd, lilbuf, 20);
+	write(new1, buffer, count);
+}
